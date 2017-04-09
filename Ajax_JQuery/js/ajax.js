147e293b659af289a56c6cc6054a30aff0662b49
Krzysztof Burkot <buryPL@gmail.com>
2017-04-09 14:50:35 +0200

    AJAX_warsztaty_zadanie2
    Komunikacja z serwerem za pomocą AJAX przy użyciu JQuery

--- Ajax_JQuery/js/ajax.js
+++ Ajax_JQuery/js/ajax.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function ajax(ajaxOptions) {
     
     //parametry połączenia i jego typu
@@ -106,4 +106,35 @@ function pobierzDane(event) {
 //        document.getElementById("dane").innerHTML = "User ID: " + jsonObj.userId + '\n\n' + "User Name: " + jsonObj.userName + "\n" + "User URL: " + jsonObj.userURL;
     }
 });
-};
+};*/
+
+$(document).ready(function () {
+    $('#batton').click( function(){         $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function (data) {
+            console.log(data);
+
+            //        var newDiv = document.createElement("div");
+            //        
+            //        var paragraf1 = document.createElement("p");
+            //        var paragraf2 = document.createElement("p");
+            //        var paragraf3 = document.createElement("p");
+            //        var paragraf4 = document.createElement("p");
+            //        
+            //        var userIdVariable = document.createTextNode("User ID: " + data.userId);
+            //        var userNameVariable = document.createTextNode("User Name: " + data.userName);
+            //        var userUrlVariable = document.createTextNode("User URL: " + data.userURL);
+            //        var separator = document.createTextNode("-------------------");
+            //        
+            //        paragraf1.appendChild(userIdVariable);
+            //        paragraf2.appendChild(userNameVariable);
+            //        paragraf3.appendChild(userUrlVariable);
+            //        paragraf4.appendChild(separator);
+            //        
+            //        newDiv.appendChild(paragraf1);
+            //        newDiv.appendChild(paragraf2);
+            //        newDiv.appendChild(paragraf3);
+            //        newDiv.appendChild(paragraf4);
+            //        
+            //        document.body.insertBefore(newDiv, document.getElementById("batton").nextSibling);
+        }); 
+    });
+});
